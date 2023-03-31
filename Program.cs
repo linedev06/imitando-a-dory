371c939c43850cca718cae20be1d5fbd4c915360
linedev06 <vitorafelismino@gmail.com>
2023-03-31 19:47:56 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("---Imitando a Dory---");
+
+Console.Write("Escreva uma Frase: ");
+string frase = Console.ReadLine()!;
+
+string baleies = frase 
+.Replace("a", "aaa")
+.Replace("A", "AAA")
+.Replace("e", "eee")
+.Replace("E", "EEE")
+.Replace("i", "iii")
+.Replace("I", "III")
+.Replace("o", "ooo")
+.Replace("O", "OOO")
+.Replace("u", "uuu")
+.Replace("U", "UUU");
+
+Console.WriteLine($"\nEm baleies: {baleies}");
